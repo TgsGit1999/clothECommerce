1c8aa806f7a1d25efb6466e14684b3eb9c49d4ab
Tomas Sosa <tomas.gabriel.sosa99@gmail.com>
2020-06-17 13:34:39 -0300

    arreglo de ogin y registro

--- Esquemas/EsquemaUsuario.js
+++ Esquemas/EsquemaUsuario.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose')
 
 
 const EsquemaUsuario = new mongoose.Schema({
-    password:{type: String,required:[true,"el password es requerido"]},
     email:{type: String,required:[true,"el email es requerido"]},
     nombreCompleto:{type: String,required:[true,"el nombre es requerido"]},
-    age:{type:String,required:[true,"la edad es requerida"]},
+    password:{type: String,required:[true,"el password es requerido"]},
+    fechaNacimiento:{type:String,required:[true,"la edad es requerida"]},
+    direccionCalle:{type: String,required:[true,"la direccion (calle) es requerida"]},
+    direccionNumero:{type: String,required:[true,"la direccion (numero) es requerida"]},
+    codigoPostal:{type: String,required:[true,"el codigo postal es requerido"]},
+    documento:{type: String,required:[false]},
+    telefonoCelular:{type: String,required:[true,"el numero de celular es requerido"]},
     registro:{type: Date,required:[true,"la fecha de registro de usuario es requerida"]},
-    direccion:{type: String,required:[false]},
-    celular:{type: String,required:[true,"el numero de celular es requerido"]},
-    telefono:{type: String,required:[false]},
-    documento:{type: String,required:[false]}
 })
 
 module.exports = EsquemaUsuario
--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const app = express()
+const bodyParser  = require('body-parser')
 require('dotenv').config();
+const cors = require('cors')
 const {initDatabase} = require('./database')
+const {RegistrarUsuario,Loggear} = require('./Controller/ControllerUsuario')
+
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(cors())
+
 
 
 const port = process.env.PORT
 
 
-app.listen({port}, () => {
-    initDatabase()
+app.listen({port}, async () => {
+     await initDatabase()
+
+
+    app.post('/registrarse', async function registrarse(req,res){
+        res.send(await RegistrarUsuario(req.body.registro)) 
+    })
+
+
+    app.post('/loguearse', async function loggearse(req,res){
+        res.send(await Loggear(req.body.login))
+    })
+
 })

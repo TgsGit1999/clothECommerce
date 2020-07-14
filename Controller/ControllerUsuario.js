504d5e4544548000625da9394c4526a9f6b0654b
Tomas Sosa <tomas.gabriel.sosa99@gmail.com>
2020-07-14 00:17:42 -0300

    la re calcada concha de tu tio

--- Controller/ControllerUsuario.js
+++ Controller/ControllerUsuario.js
@@ -1,42 +1,53 @@
-const mongoose = require('mongoose')
-const EsquemaUsuario = require('./../Esquemas/EsquemaUsuario')
-const Usuario = mongoose.model('Usuario',EsquemaUsuario,'Usuarios')
+const mongoose = require("mongoose");
+const EsquemaUsuario = require("./../Esquemas/EsquemaUsuario");
+const Usuario = mongoose.model("Usuario", EsquemaUsuario, "Usuarios");
 
-async function RegistrarUsuario({email,nombreCompleto,password,fechaNacimiento,direccionCalle,direccionNumero,codigoPostal,documento,telefonoCelular,ciudad}){
-    if(await Usuario.findOne({'email':email,'documento':documento}) != null){
-        console.log("usuario ya existente")
-        return false
-    }else{
-            new Usuario({
-                email,
-                nombreCompleto,
-                password,
-                fechaNacimiento,
-                direccionCalle,
-                direccionNumero,
-                codigoPostal,
-                documento,
-                telefonoCelular,
-                ciudad,
-                registro:Date.now()
-            }).save().then(() => {
-                return true
-            }).catch((error) => {
-                return error
-            })
-
-    }
+async function RegistrarUsuario({
+  email,
+  nombreCompleto,
+  password,
+  fechaNacimiento,
+  direccionCalle,
+  direccionNumero,
+  codigoPostal,
+  documento,
+  telefonoCelular,
+  ciudad,
+}) {
+  if ((await Usuario.findOne({ email: email, documento: documento })) != null) {
+    console.log("usuario ya existente");
+    return false;
+  } else {
+    new Usuario({
+      email,
+      nombreCompleto,
+      password,
+      fechaNacimiento,
+      direccionCalle,
+      direccionNumero,
+      codigoPostal,
+      documento,
+      telefonoCelular,
+      ciudad,
+      registro: Date.now(),
+    })
+      .save()
+      .then(() => {
+        return true;
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
 }
 
-
-
-async function Loggear({email,password}){
-    return  await Usuario.findOne({'email':email,'password':password})
+async function Loggear({ email, password }) {
+  return await Usuario.findOne({ email: email, password: password });
 }
 
 const FuncionesDeUsuario = {
-    RegistrarUsuario,
-    Loggear
-}
+  RegistrarUsuario,
+  Loggear,
+};
 
 module.exports = FuncionesDeUsuario;
--- server.js
+++ server.js
@@ -1,33 +1,25 @@
-const express = require('express')
-const app = express()
-const bodyParser  = require('body-parser')
-require('dotenv').config();
-const cors = require('cors')
-const {initDatabase} = require('./database')
-const {RegistrarUsuario,Loggear} = require('./Controller/ControllerUsuario')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
+const { initDatabase } = require("./database");
+const { RegistrarUsuario, Loggear } = require("./Controller/ControllerUsuario");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
+const port = process.env.PORT;
 
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
-app.use(cors())
+app.listen({ port }, async () => {
+  await initDatabase();
 
+  app.post("/registrarse", async function registrarse(req, res) {
+    res.send(await RegistrarUsuario(req.body.registro));
+  });
 
-
-const port = process.env.PORT
-
-
-app.listen({port}, async () => {
-     await initDatabase()
-
-
-    app.post('/registrarse', async function registrarse(req,res){
-        res.send(await RegistrarUsuario(req.body.registro)) 
-    })
-
-
-    app.post('/loguearse', async function loggearse(req,res){
-        res.send(await Loggear(req.body.login))
-    })
-
-})
+  app.post("/loguearse", async function loggearse(req, res) {
+    res.send(await Loggear(req.body.login));
+  });
+});
